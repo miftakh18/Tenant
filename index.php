932b149ae1288b0f7b683971465df1a1942ae2df
unknown <DELL VOSTRO@D99PC-SIRS1.kosala.rsmmc.co.id>
2025-05-06 19:48:23 +0700

    update css

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard Sidebar Menu |</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="plugin/fontawesome6/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> -->
 </head>
 
 <body>
@@ -15,10 +17,21 @@
                 <span class="image">
                     <img src="logo.png" alt="logo">
                 </span>
+
+                <div class="text header-text">
+                    <span class="name">CodingLab</span>
+                    <span class="profession">Web developer</span>
+                </div>
+                <i class="fa-solid fa-angle-right toggle" style="font-size: 10px;"></i>
+
             </div>
         </header>
 
     </nav>
+
+    <script src="plugin/fontawesome6/js/fontawesome.min.js"></script>
+
+
 </body>
 
 </html>
